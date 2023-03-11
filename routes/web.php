fecd051d41a665215cd1c8cbbc1a52cd06f2c207
Al Rony <alrony@sigmasolutions.com.bd>
2023-03-11 11:12:51 +0600

    2

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', [HomeController::class,'index']);
 
+Route::get('/about', function () {
+    return view('about');
+});
 
-
+Route::get('/', [HomeController::class, 'index']);
